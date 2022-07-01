192b216265e596c228bd08bbeeef2d348d18f9d0
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2022-07-01 14:07:23 -0400

    🚧 Fixed "DL" command

--- fox.sh
+++ fox.sh
@@ -13,7 +13,7 @@ bold="\033[1m"; italic="\033[3m"; underline="\033[4m"; strikethrough="\033[9m"; 
 
 foxpath="$HOME/.foxcmd"
 del=0.01
-ver="4.6.7"
+ver="4.6.8"
 if [ -z "$1" ]; then
   echo -e ""
   echo -e "🦊 FoxCMD v$ver"
@@ -231,9 +231,9 @@ if [ "$1" == "dl" ]; then
     foxint-install package ytdlp
   fi
   read -p "🎥 Please enter YouTube URL: " yturl
-  if [ "$yturl" == *'/playlist?list='* ]; then
+  if [[ "$yturl" == *'/playlist?list='* ]]; then
     echo -e "${color_blue}📄 Playlist detected. Which items do you want to download?"
-    read -p "Format: \"first:last\" OR \"all\"" playlistitems
+    read -p "Format: \"first:last\" OR \"all\" Items: " playlistitems
     if [ "$playlistitems" == "all" ]; then
       ytdlp -q --progress -f mp4 --embed-thumbnail -o '%(title)s.%(ext)s' "$yturl"
       echo -e "${color_green}✅ Saved all playlist items to your home folder!"
